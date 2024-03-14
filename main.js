94966ba5a9b83e35936bcf668cd2acee78627348
0xgrowth <sapientiakonzept@gmail.com>
2024-03-14 01:46:50 +0100

    second commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function horizontalMove() {
+  const rightlines = document.getElementById("right");  
+  const leftlines = document.getElementById("left");  
+  
+}
+
+// window.addEventListener('scroll', () => {
+//   const scrollPos = window.scrollY;
+//   const divPos = divElement.getBoundingClientRect().top;
+//   const diff = divPos - scrollPos;
+
+//   // Linear interpolation
+//   const progress = Math.min(1, Math.max(0, scrollPos / window.innerHeight));
+
+//   // Move the div by 50% of the difference
+//   divElement.style.transform = `translateY(${diff / 2 * progress}px)`;
+//});
